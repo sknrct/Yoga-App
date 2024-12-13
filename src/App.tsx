5c8631bdafed2cb425f2d06897032b335c3e0bbd
Thibault Boucard Garda <thibault.boucardgarda@gmail.com>
2024-12-13 18:59:41 +0100

    Add Benefits Page, Add Our Classes Page but not finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/scenes/navbar";
-import Home from "@/scenes/home"
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -27,6 +29,8 @@ function App() {
     setSelectedPage={setSelectedPage}
     />
     <Home setSelectedPage={setSelectedPage} />
+    <Benefits setSelectedPage={setSelectedPage} />
+    <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 };
--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,4 +1,3 @@
-
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "@/shared/types";
 import ActionButton from "@/shared/ActionButton";
